363eb47a3d515553cc8563d4463465070f5bed04
Fabricio Araujo <fabricio.info2@gmail.com>
2019-06-04 06:25:38 -0300

    'Aula de condicionais'

---
+++ aula-11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 50
+console.log(`A velocidade do seu carro é de ${vel} Km/h`)
+// condição simples
+if(vel > 60) {
+    console.log('Você está acima da velocidade permitida: MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança')
