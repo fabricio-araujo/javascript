5b4c179e922a57b3dd28aa10317c30776cc9ee69
Fabricio Araujo <fabricio.info2@gmail.com>
2019-06-22 23:45:33 -0300

    'final exercício'

--- aula-12ex/aula-13ex/js/script.js
+++ aula-12ex/aula-13ex/js/script.js
@@ -2,18 +2,21 @@ function carregar() {
     var msg = window.document.getElementById('msg')
     var img = window.document.getElementById('imagem')
     var data = new Date()
-    // var hora = data.getHours()
-    var hora = 7
+    var hora = data.getHours()
+    //var hora = 20
     msg.innerHTML = `Agora são ${hora} horas`
     if (hora >= 0 && hora < 12) {
         //BOM DIA
-        img.src = 'fotomanha.jpg'
-    } else if (hora >= 12 && hora < 18) {
+        img.src = './img/fotomanha.jpg'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora <= 18) {
         //BOA TARDE
-        img.src = 'fototarde.jpg'
+        img.src = './img/fototarde.jpg'
+        document.body.style.background = '#b9846f'
     } else {
         //BOA NOITE
-        img.src = 'fotonoite.jpg'
+        img.src = './img/fotonoite.jpg'
+        document.body.style.background = '#515154'
     }
 }
 
